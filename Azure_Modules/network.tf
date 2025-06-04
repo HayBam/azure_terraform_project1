ac4a98a5b01108b083ee0bda90a09d7abc6f403a
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-04 18:49:32 +0000

    Add resource group name

--- Azure_Modules/network.tf
+++ Azure_Modules/network.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "shola-vnet"{
 
 resource "azurerm_subnet" "subnet-ip-address-space" {
   name = "subnet-ip-address-space"
-  resource_group_name = azurerm_virtual_network.shola-vnet.name
+  resource_group_name = azurerm_resource_group.shola_project.name
   virtual_network_name = azurerm_virtual_network.shola-vnet.name
   address_prefixes = var.subnet-ip-address-space
 }
