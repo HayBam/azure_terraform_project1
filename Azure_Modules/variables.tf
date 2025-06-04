a045d6649818092e13b051898f8125e0a9b27b1a
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-04 18:45:25 +0000

    Added ip address variables

--- Azure_Modules/variables.tf
+++ Azure_Modules/variables.tf
@@ -1,3 +1,11 @@
 variable "subscription_id" {
   description = "This is the default subscription where the resources will be created"
 }
+
+variable "vnet-ip-address-space" {
+  description = "vnet address space"
+}
+
+variable "subnet-ip-address-space" {
+  description = "subnet address space"
+}
