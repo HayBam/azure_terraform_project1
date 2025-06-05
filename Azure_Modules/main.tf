5f421d99f0e6b81ee45e209e25577db18277d436
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-05 17:48:00 +0000

    Updated VM size

--- Azure_Modules/main.tf
+++ Azure_Modules/main.tf
@@ -1,6 +1,7 @@
-resource "azurerm_virtual_machine" "test-vm" {
+resource "azurerm_virtual_machine" "test-vm"{
     name = "test-vm"
-    address_space = var.subnet-ip-address-space
     location = azurerm_resource_group.shola_project.location
     resource_group_name = azurerm_resource_group.shola_project.name
+    network_interface_ids = [azurerm_network_interface.main.id]
+    vm_size = "Standard_DS1_v2"
 }
