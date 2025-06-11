aba55840cf05e1243cdf0d994bc86fab7276a81e
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-11 01:56:13 +0000

    Added nsg, public ip and nsg association block

--- Azure_Modules/network.tf
+++ Azure_Modules/network.tf
@@ -17,14 +17,48 @@ resource "azurerm_subnet" "subnet-ip-address-space" {
   address_prefixes = var.subnet-ip-address-space
 }
 
+resource "azurerm_public_ip" "public-ip" {
+  name                = "public-ip"
+  location            = azurerm_resource_group.shola_project.location
+  resource_group_name = azurerm_resource_group.shola_project.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = azurerm_resource_group.shola_project.location
+  resource_group_name = azurerm_resource_group.shola_project.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+
 resource "azurerm_network_interface" "main" {
   name                = "main-nic"
   location            = azurerm_resource_group.shola_project.location
   resource_group_name = azurerm_resource_group.shola_project.name
 
   ip_configuration {
-    name                          = "vm-config"
+    name                          = "main-nic-config"
     subnet_id                     = azurerm_subnet.subnet-ip-address-space.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public-ip.id
   }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
