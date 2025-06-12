687744452055e9246345e2e463d6a3b781e83f1f
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-12 12:42:06 +0000

    Updated vm size

--- Azure_Modules/main.tf
+++ Azure_Modules/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_machine" "vm-test" {
   location = azurerm_resource_group.shola_project.location
   resource_group_name = azurerm_resource_group.shola_project.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size = "Standard_DS1_v2"
+  vm_size = "Standard_A1_v2"
 
   storage_os_disk {
     name = "myOsDisk"
