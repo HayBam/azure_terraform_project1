804d31523cf27d214de42967b0625fb4ca17d09e
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-14 18:13:23 +0000

    Moved files to a different folder

--- Azure_Modules/resource_group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "shola_project" {
-  name = "shola_project"
-  location = "Canada Central"
-}
--- Azure_Modules/subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "subnet-ip-address-space" {
-  name = "shola-subnet"
-  address_space = var.subnet-ip-address-space
-  location = azurerm_resource_group.shola_project.location
-  resource_group_name = azurerm_resource_group.shola_project.name
-}
