35f2bb5742c2a24ffa0d38b5422241e3882831b0
HayBam <105975617+HayBam@users.noreply.github.com>
2025-06-15 02:40:07 +0000

    Added username and password variables

--- Azure_Modules/azure-ubuntu-vm/variables.tf
+++ Azure_Modules/azure-ubuntu-vm/variables.tf
@@ -2,6 +2,17 @@ variable "subscription_id" {
   description = "This is the default subscription where the resources will be created"
 }
 
+variable "resource_group_name" {
+  description = "name of resource group"
+  type = string
+  default = "shola-project"
+}
+
+variable "location" {
+  description = "azure region"
+  type = string
+  default = "Canada Central"
+}
 variable "vnet-ip-address-space" {
   description = "vnet address space"
 }
@@ -10,8 +21,14 @@ variable "subnet-ip-address-space" {
   description = "subnet address space"
 }
 
-variable "username" {
+variable "admin_username" {
   type        = string
   description = "The username for the local account that will be created on the new VM."
   default     = "azureadmin"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Admin password for the new VM."
+  sensitive = true
 }
